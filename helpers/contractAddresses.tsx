901af4a93dca9fed1759f9e6dc97d122978118d7
ocular <jywbawlo@gmail.com>
2023-10-29 06:50:28 +0100

    change contract addresses

--- helpers/contractAddresses.tsx
+++ helpers/contractAddresses.tsx
@@ -1,10 +1,10 @@
 //Hardhat
-export const EASCoinAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const EACMaster = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-export const EASNFT = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+// export const EASCoinAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// export const EACMaster = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+// export const EASNFT = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 
 //Sepolia
-// export const EASCoinAddress = "0x9e21b564a15558b9d2f60bca706399dd4ba26ca7";
-// export const EACMaster = "0x711005a50e5a6c8421058ad006763a77f956cfbb";
-// export const EASNFT = "0xc8f6b601d88454ea4f618487078fcbaf8891e512";
+export const EASCoinAddress = "0x9e21b564a15558b9d2f60bca706399dd4ba26ca7";
+export const EACMaster = "0x711005a50e5a6c8421058ad006763a77f956cfbb";
+export const EASNFT = "0xc8f6b601d88454ea4f618487078fcbaf8891e512";
